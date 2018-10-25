de28ec6df9fe890765a72ebc3cfb7027ba7329a5
Brian C. Lindner <brian@bclindner.com>
2018-10-25 00:14:36 -0400

    extra options now available

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ const discord = require('./lib/discord.js')
 const groupme = require('./lib/groupme.js')
 const pushover = require('./lib/pushover.js')
 
-async function sendMessage (msg, options) {
+async function send (msg, config, options = {}) {
   let responses = []
-  if (options.discord) {
-    responses.concat(discord(msg, options.discord))
+  if (config.discord) {
+    if ('discord' in options) {
+      responses.concat(discord(msg, config.discord, options.discord))
+    } else {
+      responses.concat(discord(msg, config.discord))
+    }
   }
-  if (options.groupme) {
-    responses.concat(groupme(msg, options.groupme))
+  if (config.groupme) {
+    if ('groupme' in options) {
+      responses.concat(groupme(msg, config.groupme, options.groupme))
+    } else {
+      responses.concat(groupme(msg, config.groupme))
+    }
   }
-  if (options.pushover) {
-    responses.concat(pushover(msg, options.pushover))
+  if (config.pushover) {
+    if ('pushover' in options) {
+      responses.concat(pushover(msg, config.pushover, options.pushover))
+    } else {
+      responses.concat(pushover(msg, config.pushover))
+    }
   }
   return Promise.all(responses)
 }
 
 module.exports = {
-  send: sendMessage,
+  send: send,
   discord: discord,
   groupme: groupme,
   pushover: pushover
